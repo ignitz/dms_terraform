44686c702e37281ed550f3809fdfdfdeb5d3e2fa
Yuri Niitsuma <ignitzhjfk@gmail.com>
2021-01-26 15:33:47 -0300

    List in variables

--- main.tf
+++ main.tf
@@ -3,37 +3,33 @@ data "aws_region" "current" {}
 module "dms" {
   source = "./dms"
 
-  # Endpoints params
+  count = length(var.servers)
+
   service_name     = var.service_name
   env              = var.env
   region           = data.aws_region.current.name
-  server_name      = var.server_name
-  database_port    = var.database_port
-  database_name    = var.database_name
-  username         = var.username
-  password         = var.password
-  ssl_mode         = var.ssl_mode
-  bucket_name      = var.bucket_name
-  bucket_folder    = var.bucket_folder
-  compression_type = var.compression_type
+
+  # Endpoints params
+  server_name      = var.servers[count.index].server_name
+  database_port    = var.servers[count.index].database_port
+  database_name    = var.servers[count.index].database_name
+  username         = var.servers[count.index].username
+  password         = var.servers[count.index].password
+  ssl_mode         = var.servers[count.index].ssl_mode
+  bucket_name      = var.servers[count.index].bucket_name
+  bucket_folder    = var.servers[count.index].bucket_folder
+  compression_type = var.servers[count.index].compression_type
 
   # Replication instance params
-  replication_instance_class = "dms.t2.micro"
-  allocated_storage          = 50
-  availability_zone          = "${data.aws_region.current.name}a"
-  publicly_accessible        = false
-  subnet_ids                 = var.subnet_ids
-  vpc_security_group_ids     = var.vpc_security_group_ids
+  replication_instance_class = var.servers[count.index].replication_instance_class
+  allocated_storage          = var.servers[count.index].allocated_storage
+  availability_zone          = var.servers[count.index].availability_zone
+  publicly_accessible        = var.servers[count.index].publicly_accessible
+  subnet_ids                 = var.servers[count.index].subnet_ids
+  vpc_security_group_ids     = var.servers[count.index].vpc_security_group_ids
 
   # Replication task params
   # Use like SQL with % to include multiple tables
-  schema_name = "public"
-  table_name  = "%"
-}
-
-# To autostart this tasks
-# https://github.com/hashicorp/terraform-provider-aws/issues/2083
-
-output "data" {
-  value = module.dms
+  schema_name = var.servers[count.index].schema_name
+  table_name  = var.servers[count.index].table_name
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,27 @@
 variable "service_name" {}
-variable "database_name" {}
 variable "env" {}
-variable "server_name" {}
-variable "database_port"{}
-variable "username" {}
-variable "password" {}
-variable "ssl_mode" {}
-variable "bucket_name" {}
-variable "bucket_folder" {}
-variable "compression_type" {}
-variable "subnet_ids" {}
-variable "vpc_security_group_ids" {}
+
+variable "servers" {
+    type = list(object({
+        server_name = string
+        database_port = number
+        database_name = string
+        username = string
+        password = string
+        ssl_mode = string
+        bucket_name = string
+        bucket_folder = string
+        compression_type = string
+
+        replication_instance_class = string
+        allocated_storage = number
+        availability_zone = string
+        publicly_accessible = bool
+        subnet_ids = list(string)
+        vpc_security_group_ids = list(string)
+
+        schema_name = string
+        table_name = string
+    }))
+}
+
